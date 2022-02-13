161faac1694dd9fdb3bcd23b03213e8cb0aa7877
Janagiramanm <janagiraman@netiapps.com>
2022-02-13 15:44:48 +0530

    debug

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,6 +16,7 @@ class AuthController extends Controller
         
         if (!Auth::attempt($request->only('email', 'password'))) {
             return response()->json([
+                'status' => 0,
                 'message' => 'Invalid login details'
                 ], 401);
             }
