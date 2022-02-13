6bbc31bcb8b12e374af6242b2fc007c0f21d5764
Janagiramanm <janagiraman@netiapps.com>
2022-02-13 10:35:51 +0530

    debug

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -25,7 +25,7 @@ class AuthController extends Controller
        
         $token = $user->createToken('auth_token')->plainTextToken;
 
-        print_r($user);exit;
+        // print_r($user);exit;
         return response()->json([
                 'access_token' => $token,
                 'token_type' => 'Bearer',
