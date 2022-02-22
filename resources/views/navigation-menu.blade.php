bb9710143cdaff344b5a3c5b21df2d048ac8f82b
kubulubabu <kubulu@netiapps.com>
2022-02-23 00:14:50 +0530

    Changes

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -7,7 +7,7 @@
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
                         <x-jet-application-mark class="block h-9 w-auto" />
-                        
+
                     </a>
                 </div>
 
@@ -135,7 +135,35 @@
             </div>
         </div>
     </div>
+        <ul class="flex new-nav-list">
+            <li class="mr-3 mt-2 flex-1">
+                <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                    <i class="fas fa-tachometer-alt pr-0 md:pr-3"></i></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">Dashboard</span>
+                </x-jet-nav-link>
 
+            </li>
+            <li class="mr-3 flex-1">
+                <x-jet-nav-link href="{{ route('roles') }}" :active="request()->routeIs('roles')">
+                    <i class="fas fa-user-tag pr-0 md:pr-3"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">Roles</span>
+                </x-jet-nav-link>
+            </li>
+            <li class="mr-3 mt-2 flex-1">
+                <x-jet-nav-link href="{{ route('users') }}" :active="request()->routeIs('users')">
+                    <i class="fas fa-users pr-0 md:pr-3"></i></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">Users</span>
+                </x-jet-nav-link>
+            </li>
+            <li class="mr-3 mt-2 flex-1">
+                <x-jet-nav-link href="{{ route('services') }}" :active="request()->routeIs('services')">
+                    <i class="fas fa-address-card pr-0 md:pr-3"></i></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">Services</span>
+                </x-jet-nav-link>
+            </li>
+            <li class="mr-3 mt-2 flex-1">
+                <x-jet-nav-link href="{{ route('voters') }}" :active="request()->routeIs('voters')">
+                    <i class="fas fa-address-card pr-0 md:pr-3"></i></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">Voters</span>
+                </x-jet-nav-link>
+            </li>
+
+        </ul>
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
