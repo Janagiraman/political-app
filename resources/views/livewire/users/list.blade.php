35cd3a82c60aa9e435893a7caf179df8f0969995
kubulubabu <kubulu@netiapps.com>
2022-02-23 15:45:13 +0530

    Changes

--- resources/views/livewire/users/list.blade.php
+++ resources/views/livewire/users/list.blade.php
@@ -2,35 +2,35 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Users') }}
         </h2>
-  
+
 </x-slot>
 
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
-       
+
         @if($updateMode)
             @include('livewire.users.create')
         @elseif($createMode)
             @include('livewire.users.create')
         @else
-            <table class="table-fixed w-full">                
-                          
+            <table class="table-fixed w-full">
+
                             <x-jet-secondary-button wire:click="create()" class=" float-right bg-orange-500 hover:bg-gray-300 hover:text-white-100 px-4 py-2 my-6">
                                         Add New User
                             </x-jet-button>
-                    
+
                             <thead>
                                 <tr class="bg-gray-100">
-                                    <th class="px-4 py-2 w-20">No.</th>
-                                    <th class="px-4 py-2 w-24">Emp Code</th>
-                                    <th class="px-4 py-2">Name</th>
-                                    <th class="px-4 py-2">Email</th>                                   
-                                    <th class="px-4 py-2">Mobile</th>                                   
-                                    <th class="px-4 py-2">IMEI</th>                                   
-                                    <th class="px-4 py-2">Role</th>                                   
-                                    <th class="px-4 py-2 w-24">Action</th>
-                                </tr>   
+                                    <th class="px-4 py-2 w-20 text-left">No.</th>
+                                    <th class="px-4 py-2 w-24 text-left">Emp Code</th>
+                                    <th class="px-4 py-2 text-left">Name</th>
+                                    <th class="px-4 py-2 text-left">Email</th>
+                                    <th class="px-4 py-2 text-left">Mobile</th>
+                                    <th class="px-4 py-2 text-left">IMEI</th>
+                                    <th class="px-4 py-2 text-left">Role</th>
+                                    <th class="px-4 py-2 w-24 text-left">Action</th>
+                                </tr>
                             </thead>
                             <tbody>
                             @php $no = 1; @endphp
@@ -61,16 +61,16 @@
                     <x-slot name="title">
                         {{ __('Delete Item') }}
                     </x-slot>
-            
+
                     <x-slot name="content">
                         {{ __('Are you sure you want to delete Item? ') }}
                     </x-slot>
-            
+
                     <x-slot name="footer">
                         <x-jet-secondary-button wire:click="$set('confirmingItemDeletion', false)" wire:loading.attr="disabled">
                             {{ __('Cancel') }}
                         </x-jet-secondary-button>
-            
+
                         <x-jet-danger-button class="ml-2" wire:click="deleteItem({{ $confirmingItemDeletion }})" wire:loading.attr="disabled">
                             {{ __('Delete') }}
                         </x-jet-danger-button>
